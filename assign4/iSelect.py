5b8ad6cd1c15c2edf015d6dc2979e539204fc0a0
Timothy Chisholm <tgctis@rit.edu>
2019-03-08 07:59:21 -0500

    HW4 Done

--- assign4/iSelect.py
+++ assign4/iSelect.py
@@ -1,27 +1,36 @@
 def iSelect(xs, i):
-    return iSelectHelper(xs, i, 0, len(xs))
+    return iSelectHelper(xs, i, 0, len(xs)-1)
 
-# Replace with in-place algo from note papers
-def p(x, ys=[], l=[], g=[]):
-    if len(ys) < 1:
-        return [l, g]
-    else:
-        y = ys.pop()
-        if y <= x:
-            l.append(y)
-            return p(x, ys, l, g)
-        elif y > x:
-            g.append(y)
-            return p(x, ys, l, g)
+
+def p(a, l, h):
+    x = a[h]
+    i = l-1
+    j = l
+    while j < h:
+        if a[j] <= x:
+            i = i+1
+            t = a[i]
+            a[i] = a[j]
+            a[j] = t
+        j = j+1
+    t = a[i+1]
+    a[i+1] = a[h]
+    a[h] = t
+    return i+1
 
 
 def iSelectHelper(xs, i, l, g):
-    x = xs[l]
-    p(x, xs, l, g)
-    return l+g
+    j = p(xs, l, g)
+
+    if i < j:
+        return iSelectHelper(xs, i, l, j-1)
+    if i > j:
+        return iSelectHelper(xs, i, j+1, g)
+    else:
+        return xs[j]
 
 
 xs = [0, 2, 7, 9, 55]
-i = 3
+i = 1
 
 print iSelect(xs, i)
