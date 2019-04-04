2fd61a2a2c6a93db1480793381dcec073144f0fc
Timothy Chisholm <tgctis@rit.edu>
2019-04-04 18:47:19 -0400

    Question 5 done.

--- assign5/printPar.py
+++ assign5/printPar.py
@@ -2,16 +2,9 @@ import sys
 
 
 def extraSpace(S, M, i, j):
-    if len(S) < 1 or i > j or i > len(S):
+    if len(S) < 1 or i > j or i >= len(S):
         return M
-    # print S[i], len(S[i]) + 1
     return extraSpace(S, M-(len(S[i])+1), i+1, j)
-
-
-def mn(S, M, i):
-    if i > len(S) or extraSpace(S[:i], M, 0, len(S[:i])-1) < 0:
-        return i-1
-    return mn(S, M, i+1)
 
 
 def badnessLine(S, M, i, j):
@@ -21,25 +14,66 @@ def badnessLine(S, M, i, j):
     return M
 
 
-# Not working...
 def minBad(S, M, i):
-    j = mn(S, M, i)
-    for n in range(0, j):
-        print S[n],
+    j = i
+    while j < len(S) and extraSpace(S, M, i, j) >= 0:
+        j += 1
+    if extraSpace(S, M, i, len(S)-1) >= 0:
+        # print j, S[i:j], badnessLine(S[i:], M, 0, len(S[i:]) - 1)
+        return badnessLine(S[i:j], M, 0, len(S)-1)
+    # print j, S[i:j], badnessLine(S[i:j], M, 0, j-i-1)
+    return badnessLine(S[i:j], M, 0, j-i-1) + minBad(S, M, j)
 
-    if extraSpace(S, M, 0, len(S)-1) >= 0:
-        print "| Badness = ", badnessLine(S, M, 0, len(S)-1)
-        return badnessLine(S, M, 0, len(S)-1)
-    print "| Badness = ", badnessLine(S[:j], M, 0, j - 1)
-    return badnessLine(S[:j], M, 0, j-1) + minBad(S[j:], M, 0)
+
+L = []
+def minBadDynamic(S, M):
+    if len(S) == 0:
+        badness = 0
+        for l in L:
+            badness += l
+        return badness
+    i = 0
+    j = i
+    while j < len(S) and extraSpace(S, M, i, j) >= 0:
+        j += 1
+    L.append(badnessLine(S[i:j], M, 0, len(S[i:j])))
+    return minBadDynamic(S[j:], M)
+
+
+def minBadDynamicChoice(S, M):
+    if len(S) == 0:
+        badness = 0
+        choices = []
+        for l in L:
+            badness += l[0]
+            choices.append(l[1])
+        return badness, choices
+    i = 0
+    j = i
+    while j < len(S) and extraSpace(S, M, i, j) >= 0:
+        j += 1
+    L.append([badnessLine(S[i:j], M, 0, len(S[i:j])), j])
+    return minBadDynamicChoice(S[j:], M)
+
+
+def printParagraph(S, M):
+    lines = minBadDynamicChoice(S, M)
+    i = 0
+    for l in lines[1]:
+        j = i + l
+        line = ""
+        for n in range(i, j):
+            line += S[n] + " "
+        print line
+        i = j
+    print "Line length: %d, Paragraph Badness: %d" % (M, lines[0])
 
 
 def runner():
+    # S = ["My Spoon is too big."]
     S = ["John", "Cleese", "Eric", "Idle", "King Aurthur"]
-    # S = "1", "12", "123"
     M = 15
-    i = 0
-    print minBad(S, M, i)
-    # print mn(S, M, i)
+    printParagraph(S, M)
+
 
 runner()
